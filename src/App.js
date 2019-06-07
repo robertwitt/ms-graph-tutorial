534d867f13fde8f19c25e46febe9d6f3ebf174bc
robertwitt <robert_witt@icloud.com>
2019-06-07 11:07:09 +0200

    Create basic SPA.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import NavBar from './NavBar';
+import ErrorMessage from './ErrorMessage';
+import Welcome from './Welcome';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isAuthenticated: false,
+      user: {},
+      error: null
+    };
+  }
+
+  render() {
+    let error = null;
+    if (this.state.error) {
+      error = <ErrorMessage message={this.state.error.message} debug={this.state.error.debug} />;
+    }
+
+    return (
+      <Router>
+        <div>
+          <NavBar
+            isAuthenticated={this.state.isAuthenticated}
+            authButtonMethod={null}
+            user={this.state.user}/>
+          <Container>
+            {error}
+            <Route exact path="/"
+              render={(props) =>
+                <Welcome {...props}
+                  isAuthenticated={this.state.isAuthenticated}
+                  user={this.state.user}
+                  authButtonMethod={null} />
+              } />
+          </Container>
+        </div>
+      </Router>
+    );
+  }
+
+  setErrorMessage(message, debug) {
+    this.setState({
+      error: {message: message, debug: debug}
+    });
+  }
 }
 
 export default App;
