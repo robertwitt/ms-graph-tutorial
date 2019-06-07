63b3b36a72ff0dda8eaec38fac3621963d495ffa
robertwitt <robert_witt@icloud.com>
2019-06-07 12:25:41 +0200

    Add Azure AD authentication.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,73 @@ import NavBar from './NavBar';
 import ErrorMessage from './ErrorMessage';
 import Welcome from './Welcome';
 import 'bootstrap/dist/css/bootstrap.css';
+import config from './Config';
+import { UserAgentApplication } from 'msal';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
+    this.userAgentApplication = new UserAgentApplication(config.appId, null, null);
+
+    var user = this.userAgentApplication.getUser();
+
     this.state = {
-      isAuthenticated: false,
+      isAuthenticated: (user !== null),
       user: {},
       error: null
     };
+
+    if (user) {
+      // Enhance user object with data from Graph
+      this.getUserProfile();
+    }
+  }
+
+  async getUserProfile() {
+    try {
+      // Get the access token silently
+      // If the cache contains a non-expired token, this function
+      // will just return the cached token. Otherwise, it will
+      // make a request to the Azure OAuth endpoint to get a token
+  
+      var accessToken = await this.userAgentApplication.acquireTokenSilent(config.scopes);
+  
+      if (accessToken) {
+        // TEMPORARY: Display the token in the error flash
+        this.setState({
+          isAuthenticated: true,
+          error: { message: "Access token:", debug: accessToken }
+        });
+      }
+    }
+    catch(err) {
+      var errParts = err.split('|');
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: { message: errParts[1], debug: errParts[0] }
+      });
+    }
+  }
+
+  async login() {
+    try {
+      await this.userAgentApplication.loginPopup(config.scopes);
+      await this.getUserProfile();
+    }
+    catch (err) {
+      var errParts = err.split('|');
+      this.setState({
+        isAuthenticated: false,
+        user: {},
+        error: { message: errParts[1], debug: errParts[0] }
+      });
+    }
+  }
+
+  logout() {
+    this.userAgentApplication.logout();
   }
 
   render() {
@@ -28,8 +85,8 @@ class App extends Component {
         <div>
           <NavBar
             isAuthenticated={this.state.isAuthenticated}
-            authButtonMethod={null}
-            user={this.state.user}/>
+            authButtonMethod={this.state.isAuthenticated ? this.logout.bind(this) : this.login.bind(this)}
+            user={this.state.user} />
           <Container>
             {error}
             <Route exact path="/"
@@ -37,7 +94,7 @@ class App extends Component {
                 <Welcome {...props}
                   isAuthenticated={this.state.isAuthenticated}
                   user={this.state.user}
-                  authButtonMethod={null} />
+                  authButtonMethod={this.login.bind(this)} />
               } />
           </Container>
         </div>
@@ -47,7 +104,7 @@ class App extends Component {
 
   setErrorMessage(message, debug) {
     this.setState({
-      error: {message: message, debug: debug}
+      error: { message: message, debug: debug }
     });
   }
 }
